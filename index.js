fd7a93588e635e182cbb22c62bc2a731e4aee5f4
kajendran1451 <67456777+kajendran1451@users.noreply.github.com>
2024-08-22 18:58:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function initialFetchCloudWatchData() {
-    let baseURL = "https://tztqsv9x8f.execute-api.us-east-1.amazonaws.com/testing/yamlcloudwatchtest";
+    let baseURL = "https://cdfq5n18c3.execute-api.us-east-1.amazonaws.com/testing/yamlcloudwatchtest";
     try {
        let response = await fetch(baseURL);
         if (!response.ok) {
@@ -24,7 +24,7 @@ async function initialFetchCloudWatchData() {
 }
 
 async function customTimeFetchCloudWatchData(timeframeLength, timeframeUnit) {
-    let baseURL = "https://tztqsv9x8f.execute-api.us-east-1.amazonaws.com/testing/yamlcloudwatchtest";
+    let baseURL = "https://cdfq5n18c3.execute-api.us-east-1.amazonaws.com/testing/yamlcloudwatchtest";
     let timeframeLengthParam = `/?timeframeLength=${timeframeLength}&`;
     let timeframeUnitParam = `timeframeUnit=${timeframeUnit}`;
     let paramURL = baseURL + timeframeLengthParam + timeframeUnitParam;
